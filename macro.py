439800802bc38782f67ca0d3553983c0e4a487c4
namj <jwnam0709@gmail.com>
2021-11-01 17:00:03 -0700

    changing readme

--- macro.py
+++ macro.py
@@ -3,20 +3,20 @@ import os
 
 # You must implement 'step1_save_feature/your_feature_extraction_code.py' before you run this script.
 python_version = "python3.6"
-feature_extraction_system_name = 'SimCLRv2'
+feature_extraction_system_name = 'ResNet50' #your architecture name
 
-# saves the embedding vectors of the images to .h5 file.
-subprocess.run([python_version, 
-                os.path.join("step1_save_feature", "save_feature2h5py.py"), 
-                "--input_dir", 
-                os.path.join("data", "ShapeY200"),
-                "--output_dir",
-                os.path.join("data", "intermediate"),
-                "--run_example", 
-                "False",
-                "--name",
-                feature_extraction_system_name
-                ])
+# # saves the embedding vectors of the images to .h5 file.
+# subprocess.run([python_version, 
+#                 os.path.join("step1_save_feature", "save_feature2h5py.py"), 
+#                 "--input_dir", 
+#                 os.path.join("data", "ShapeY200"),
+#                 "--output_dir",
+#                 os.path.join("data", "intermediate"),
+#                 "--run_example", 
+#                 "True",
+#                 "--name",
+#                 feature_extraction_system_name
+#                 ])
 
 # computes correlation with the extracted embedding vectors.
 subprocess.run([python_version,
@@ -25,33 +25,33 @@ subprocess.run([python_version,
                 os.path.join("data", "intermediate", feature_extraction_system_name+".h5")
                 ])
 
-# runs nearest-neighbor benchmark analaysis
-subprocess.run([python_version,
-                os.path.join("step3_benchmark_analysis", "get_nn_classification_error_with_exclusion_distance.py"),
-                "--input_dir",
-                os.path.join("data", "intermediate", feature_extraction_system_name+".h5"),
-                "--output_dir",
-                os.path.join("data", "processed", feature_extraction_system_name+".h5")
-                ])
+# # runs nearest-neighbor benchmark analaysis
+# subprocess.run([python_version,
+#                 os.path.join("step3_benchmark_analysis", "get_nn_classification_error_with_exclusion_distance.py"),
+#                 "--input_dir",
+#                 os.path.join("data", "intermediate", feature_extraction_system_name+".h5"),
+#                 "--output_dir",
+#                 os.path.join("data", "processed", feature_extraction_system_name+".h5")
+#                 ])
 
-# graphs results (exclusion distance vs nn matching error)
-subprocess.run([python_version,
-                os.path.join("step4_graph_results", "graph_exclusion_top1error_v2.py"),
-                "--input_dir",
-                os.path.join("data", "processed", feature_extraction_system_name+".h5"),
-                "--output_dir",
-                os.path.join("figures", feature_extraction_system_name),
-                "--within_category_error",
-                "False"
-                ])
+# # graphs results (exclusion distance vs nn matching error)
+# subprocess.run([python_version,
+#                 os.path.join("step4_graph_results", "graph_exclusion_top1error_v2.py"),
+#                 "--input_dir",
+#                 os.path.join("data", "processed", feature_extraction_system_name+".h5"),
+#                 "--output_dir",
+#                 os.path.join("figures", feature_extraction_system_name),
+#                 "--within_category_error",
+#                 "False"
+#                 ])
 
-subprocess.run([python_version,
-                os.path.join("step4_graph_results", "graph_exclusion_top1error_v2.py"),
-                "--input_dir",
-                os.path.join("data", "processed", feature_extraction_system_name+".h5"),
-                "--output_dir",
-                os.path.join("figures", feature_extraction_system_name),
-                "--within_category_error",
-                "True"
-                ])
+# subprocess.run([python_version,
+#                 os.path.join("step4_graph_results", "graph_exclusion_top1error_v2.py"),
+#                 "--input_dir",
+#                 os.path.join("data", "processed", feature_extraction_system_name+".h5"),
+#                 "--output_dir",
+#                 os.path.join("figures", feature_extraction_system_name),
+#                 "--within_category_error",
+#                 "True"
+#                 ])
 
