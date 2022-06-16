c0e58bf160c1f935478d57b5ab559a32266c7863
namj <jwnam0709@gmail.com>
2022-06-16 14:38:17 -0700

    cleanup

--- macro.py
+++ macro.py
@@ -5,16 +5,25 @@ python_version = "python3.6"
 
 #runs on base shapey
 networks = [
-    'simclr_v1_resnet50x1',
-    'mixer_b16_224',
-    'resnetv2_50x1_bitm',
-    'resnetv2_50x1_bit_distilled',
-    'vit_large_patch16_224'
+    'example'
 ]
-
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 if __name__ == '__main__':
+    subprocess.run([python_version,
+                    'analysis.py',
+                    '-m',
+                    'network={}'.format(','.join(networks)),
+                    'data.project_dir={}'.format(PROJECT_DIR), 
+                    'data=ShapeY200'])
+
+    subprocess.run([python_version,
+                    'graph.py',
+                    '-m',
+                    'network={}'.format(','.join(networks)),
+                    'data.project_dir={}'.format(PROJECT_DIR),
+                    'graph=obj,cat'])
+
     #Contrast Reversed
     subprocess.run([python_version,
                     'analysis.py',
