1f3beb1f63c8fa03ededcbb05d6404f977e1c605
namj <jwnam0709@gmail.com>
2022-06-28 15:56:37 -0700

    update version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ CLASSIFIERS = [
 # calling the setup function
 setuptools.setup(
     name="ShapeY",
-    version="0.1.6",
+    version="0.1.7",
     description="Benchmark that tests shape recognition",
     long_description=long_description,
     url="https://github.com/njw0709/ShapeY",
